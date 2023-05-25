12d85afb614c3fff6933bcd267d2fe68e91723ca
gcsanad <gcsani01@gmail.com>
2023-05-25 11:48:48 +0200

    4.ora vege

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello world!");
+﻿using ConsoleApp1;
+
+//2.Feladat
+List<kutyak> kutyak = new List<kutyak>(File.ReadAllLines("F:\\Us\\Csanád\\prog\\kutyák\\ConsoleApp1\\ConsoleApp1\\Adatok").Select(x => new kutyak(x)).Skip(1).ToList());
+
+//3.Feladat
+Console.WriteLine($"3.Feladat: {kutyak.Count()}");
+
+//4.Feladat
---
+++ ConsoleApp1/kutyak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class kutyak
+    {
+        int azonosito;
+        string kutyaNeve;
+
+        public kutyak(string sor)
+        {
+            string[] felosztas = sor.Split(';');
+            this.azonosito = int.Parse(felosztas[0]);
+            this.kutyaNeve = felosztas[1];
+        }
+
+        public int Azonosito { get => azonosito;}
+        public string KutyaNeve { get => kutyaNeve;}
+    }
+}
